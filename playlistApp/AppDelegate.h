998b2a2210cf911aa8b4878c6fd3a97b907b2cf6
Carl Elbaz <carl.elbaz@gmail.com>
2015-06-27 15:46:05 -0400

    Initial Commit

---
+++ playlistApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  playlistApp
+//
+//  Created by Carl Elbaz on 6/27/15.
+//  Copyright (c) 2015 Carl Elbaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
